2d178b34d731b8c19b4c3756648f95136d7197eb
Jacob Burenstam <burenstam@gmail.com>
2015-08-09 18:35:54 +0200

    Removed dead attributes username/password in Config
    Added specs for Config

--- lib/transifex/config.rb
+++ lib/transifex/config.rb
@@ -5,8 +5,5 @@ module Transifex
   module Config
     BASE_URL = 'https://www.transifex.com'
     USER_AGENT = "transifex #{Transifex::VERSION}"
-    VALID_OPTIONS = [:username, :password]
-
-    attr_accessor *VALID_OPTIONS
   end
 end
